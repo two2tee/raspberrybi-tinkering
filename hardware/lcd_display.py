bd220354578f26812e6eaa02b355cadad40bd7e5
Dennis Thinh Tan Nguyen <dennisnguyen3000@yahoo.dk>
2024-11-08 21:53:17 +0100

    Add terminal message printing for disabled LCD display

--- hardware/lcd_display.py
+++ hardware/lcd_display.py
@@ -5,13 +5,19 @@ class LCD_Display:
     def __init__(self, is_backlight=True, is_disabled=False):
         self.is_disabled = is_disabled
         if is_disabled:
+            print("LCD is disabled")
             return
 
         self.LCD = LcdDriver(pi_rev=2, i2c_addr=0x27, backlight=is_backlight)
 
+    def printTerminalMessage(self, message, headerText=""):
+          print(f"\n---------\n{headerText}\n{message}\n---------\n")
+
+
 
     def printMessageWithDelay(self, duration_sec, message, headerText=""):
         if self.is_disabled:
+            self.printTerminalMessage(message, headerText)
             return
 
         self.printMessage(message, headerText)
@@ -20,6 +26,7 @@ class LCD_Display:
 
     def printMessage(self, message, headerText=""):
         if self.is_disabled:
+            self.printTerminalMessage(message, headerText)
             return
 
         if len(headerText) > 0:
